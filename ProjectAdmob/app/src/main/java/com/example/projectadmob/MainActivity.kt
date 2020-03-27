d936cb933bb8488862965a1ae80b1cb774280e91
Aurimas Sikorkas <aurimas@dogo.app>
2020-03-27 14:05:51 +0200

    added some ads

--- ProjectAdmob/app/src/main/java/com/example/projectadmob/MainActivity.kt
+++ ProjectAdmob/app/src/main/java/com/example/projectadmob/MainActivity.kt
@@ -1,15 +1,20 @@
 package com.example.projectadmob
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.InterstitialAd
+import com.google.android.gms.ads.MobileAds
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var mInterstitialAd: InterstitialAd
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -22,5 +27,16 @@ class MainActivity : AppCompatActivity() {
             R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+        MobileAds.initialize(this, "ca-app-pub-3487316964923013~1867650985")
+        mInterstitialAd = InterstitialAd(this)
+        mInterstitialAd.adUnitId = "ca-app-pub-3940256099942544/1033173712"
+        mInterstitialAd.loadAd(AdRequest.Builder().build())
+        findViewById<Button>(R.id.button).setOnClickListener {
+            if (mInterstitialAd.isLoaded) {
+                mInterstitialAd.show()
+            } else {
+                Log.d("TAG", "The interstitial wasn't loaded yet.")
+            }
+        }
     }
 }
